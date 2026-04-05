3237bc39ccafcb1b8bb33f3ad7d4ef6f43cf21e6
Rimac06 <jakov.rimac4@gmail.com>
2026-04-05 13:10:11 +0200

    test1

--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class TemplateMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
 	}
 }
