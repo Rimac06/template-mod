c9c6b3a3150ba4b760c15df2fd70eb2776eb8f20
rimac <jakov.rimac4@gmail.com>
2026-04-12 18:59:08 +0200

    TEST

--- src/main/java/com/example/TemplateModDataGenerator.java
+++ src/main/java/com/example/TemplateModDataGenerator.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -7,5 +8,12 @@ public class TemplateModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
+        FabricDataGenerator.Pack pack= fabricDataGenerator.createPack();
+
+        pack.addProvider(ModBlockTagProvider::new);
+        pack.addProvider(ModItemTagProvider::new);
+        pack.addProvider(ModLootTableProvider::new);
+        pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModRecipeProvider::new);
 	}
 }
